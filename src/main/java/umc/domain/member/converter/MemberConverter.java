9a9b5b7fe697fb2680fefb94b79af5bfdff86104
Kohseoyoung <hanaharu1234@naver.com>
2026-04-09 23:14:55 +0900

    4주차 과제

---
+++ src/main/java/umc/domain/member/converter/MemberConverter.java
@@ -0,0 +1,4 @@
+package umc.domain.member.converter;
+
+public class MemberConverter {
+}
--- src/test/java/umc/Umc10thApplicationTests.java
+++ src/test/java/umc/Umc10thApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ApplicationTests {
+class Umc10thApplicationTests {
 
 	@Test
 	void contextLoads() {
