6ace867b153a6b313177529e7f64281eae7207c3
ChoiMinSang <min393917@gmail.com>
2026-04-29 23:57:33 +0900

    feat: 응답 통일 객체 및 에러 핸들러 생성

--- src/main/java/com/example/umc10th/domain/member/controller/MemberController.java
+++ src/main/java/com/example/umc10th/domain/member/controller/MemberController.java
@@ -1,4 +1,28 @@
 package com.example.umc10th.domain.member.controller;
 
+import com.example.umc10th.domain.member.code.MemberSuccessCode;
+import com.example.umc10th.domain.member.service.MemberService;
+import com.example.umc10th.global.apiPayload.ApiResponse;
+import com.example.umc10th.global.apiPayload.code.BaseSuccessCode;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api")
 public class MemberController {
+
+    private final MemberService memberService;
+
+    // 마이페이지
+    @PostMapping("/v1/users/me")
+    public ApiResponse<MemberResDTO.GetInfo> getInfo(
+            @RequestBody MemberReqDTO.GetInfo dto
+    ) {
+        BaseSuccessCode code = MemberSuccessCode.OK;
+        return ApiResponse.onSuccess(code, memberService.getInfo(dto));
+    }
 }
--- src/main/java/com/example/umc10th/domain/member/dto/MemberReqDto.java
+++ src/main/java/com/example/umc10th/domain/member/dto/MemberReqDto.java
@@ -1,4 +1,8 @@
 package com.example.umc10th.domain.member.dto;
 
 public class MemberReqDto {
+
+   public record GetInfo(
+       Long id
+   ){}
 }
