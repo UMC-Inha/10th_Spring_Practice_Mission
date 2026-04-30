4d686df95e79100736770d0d4bbabd314ce584ac
yeongjun25 <ss91530441@gmail.com>
2026-04-30 16:33:43 +0900

    feat: Service, Repository 및 MemberErrorCode 추가

--- src/main/java/umc/domain/member/exception/code/MemberErrorCode.java
+++ src/main/java/umc/domain/member/exception/code/MemberErrorCode.java
@@ -1,4 +1,18 @@
 package umc.domain.member.exception.code;
 
-public enum MemberErrorCode {
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import umc.global.apiPayload.code.BaseErrorCode;
+
+@Getter
+@RequiredArgsConstructor
+public enum MemberErrorCode implements BaseErrorCode {
+
+    MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "MEMBER404_1", "해당 멤버를 찾을 수 없습니다."),
+    ;
+
+    private final HttpStatus status;
+    private final String code;
+    private final String message;
 }
--- src/main/java/umc/domain/member/service/MemberService.java
+++ src/main/java/umc/domain/member/service/MemberService.java
@@ -1,4 +1,29 @@
 package umc.domain.member.service;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import umc.domain.member.converter.MemberConverter;
+import umc.domain.member.dto.MemberRequestDTO;
+import umc.domain.member.dto.MemberResponseDTO;
+import umc.domain.member.entity.Member;
+import umc.domain.member.exception.MemberException;
+import umc.domain.member.exception.code.MemberErrorCode;
+import umc.domain.member.repository.MemberRepository;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
 public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    public MemberResponseDTO.GetInfo getInfo(
+            MemberRequestDTO.GetInfo dto
+    ) {
+       Long memberId = dto.id();
+        Member member = memberRepository.findById(memberId)
+                .orElseThrow(() -> new MemberException(MemberErrorCode.MEMBER_NOT_FOUND));
+        return MemberConverter.toGetInfo(member);
+    }
 }
