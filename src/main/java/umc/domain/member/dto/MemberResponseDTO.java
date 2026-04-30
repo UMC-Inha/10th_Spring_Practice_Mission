898015012e4138500d22474515a69cb1bcb8b9da
yeongjun25 <ss91530441@gmail.com>
2026-04-30 16:29:58 +0900

    feat: API 명세서 기반 Controller 및 DTO 추가

--- src/main/java/umc/domain/member/dto/MemberResponseDTO.java
+++ src/main/java/umc/domain/member/dto/MemberResponseDTO.java
@@ -1,4 +1,15 @@
 package umc.domain.member.dto;
 
+import lombok.Builder;
+
 public class MemberResponseDTO {
+
+    @Builder
+    public record GetInfo(
+            String name,
+            String email,
+            String phoneNumber,
+            String profileUrl,
+            Integer point
+    ) {}
 }
