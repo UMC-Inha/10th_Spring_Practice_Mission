47060146fc396dc0c78e450ffdd6e844fda26a6c
zeoueon <zeoueon129@gmail.com>
2026-04-30 21:25:26 +0900

    feat: 리뷰 도메인 리뷰 작성 API DTO, 컨트롤러, 성공코드 구현

--- src/main/java/umc/domain/review/controller/ReviewController.java
+++ src/main/java/umc/domain/review/controller/ReviewController.java
@@ -1,4 +1,22 @@
 package umc.domain.review.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import umc.domain.review.dto.ReviewReqDTO;
+import umc.domain.review.dto.ReviewResDTO;
+import umc.domain.review.exception.code.ReviewSuccessCode;
+import umc.global.apiPayload.ApiResponse;
+
+@RestController
+@RequestMapping("/api/reviews")
+@RequiredArgsConstructor
 public class ReviewController {
+
+    @PostMapping("/{memberMissionId}")
+    public ApiResponse<ReviewResDTO.ReviewCreateDTO> createReview(
+            @PathVariable Long memberMissionId,
+            @ModelAttribute ReviewReqDTO.ReviewCreateDTO reqDto
+    ) {
+        return ApiResponse.onSuccess(ReviewSuccessCode.REVIEW_CREATED, null);
+    }
 }
--- src/main/java/umc/domain/review/dto/ReviewResDTO.java
+++ src/main/java/umc/domain/review/dto/ReviewResDTO.java
@@ -1,4 +1,20 @@
 package umc.domain.review.dto;
 
+import lombok.Builder;
+
+import java.util.List;
+
 public class ReviewResDTO {
+
+    @Builder
+    public record ReviewCreateDTO(
+            Long reviewId,
+            String reviewContent,
+            Integer starRating,
+            List<Image> images
+    ) {
+        public record Image(
+                String imageUrl
+        ) {}
+    }
 }
