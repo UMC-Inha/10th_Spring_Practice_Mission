3327e63662f23381b7ce0e9ec0db5a53eb876359
andyoon <andyoon502@naver.com>
2026-04-30 23:06:25 +0900

    Feat: 홈 화면 조회 controller, DTO 추가

--- src/main/java/umc/domain/member/controller/MemberController.java
+++ src/main/java/umc/domain/member/controller/MemberController.java
@@ -1,10 +1,7 @@
 package umc.domain.member.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import umc.domain.member.dto.MemberReqDTO;
 import umc.domain.member.dto.MemberResDTO;
 import umc.domain.member.exception.code.MemberSuccessCode;
@@ -19,10 +16,16 @@ public class MemberController {
 
     private final MemberService memberService;
 
-    @PostMapping("/v1/users/me")
+    @PostMapping("/users/me")
     public ApiResponse<MemberResDTO.GetInfo> getInfo(
             @RequestBody MemberReqDTO.GetInfo dto
             ){
+        BaseSuccessCode code = MemberSuccessCode.OK;
+        return ApiResponse.onSuccess(code, null);
+    }
+
+    @GetMapping("/home")
+    public ApiResponse<MemberResDTO.Home> getHome(){
         BaseSuccessCode code = MemberSuccessCode.OK;
         return ApiResponse.onSuccess(code, null);
     }
--- src/main/java/umc/domain/member/dto/MemberResDTO.java
+++ src/main/java/umc/domain/member/dto/MemberResDTO.java
@@ -2,6 +2,9 @@ package umc.domain.member.dto;
 
 import lombok.Builder;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public class MemberResDTO {
 
     @Builder
@@ -11,5 +14,25 @@ public class MemberResDTO {
             String email,
             String phoneNumber,
             Integer point
+    ){}
+
+    @Builder
+    public record Home(
+            Long locationId,
+            String locationName,
+            Integer currentPoint,
+            Integer completedMissionCount,
+            List<HomeMission> missions
+    ){}
+
+    @Builder
+    public record HomeMission(
+            Long missionId,
+            Long storeId,
+            String storeName,
+            String storeCategory,
+            String missionContent,
+            Integer rewardPoint,
+            LocalDate dueDate
     ){}
 }
