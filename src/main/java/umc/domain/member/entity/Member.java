e35208c985b07bf2ba9d6194651f2fefd926bc3e
yeongjun25 <ss91530441@gmail.com>
2026-04-30 16:38:38 +0900

    feat: MemberConverter 및 Member 엔티티 필드 추가

--- src/main/java/umc/domain/member/entity/Member.java
+++ src/main/java/umc/domain/member/entity/Member.java
@@ -1,4 +1,39 @@
 package umc.domain.member.entity;
 
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
 public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String nickname;
+
+    private LocalDate birth;
+
+    private String address;
+
+    private String email;
+
+    private Integer point;
+
+    private String phoneNumber;
+
+    private Boolean phone_verified;
+
+    private String profileUrl;
+
+
 }
