9b98770a606f263c05626a805bb8a1e1294fd9f3
2u6in <zxcv35602667@gmail.com>
2026-05-03 04:59:14 +0900

    feat: 홈 화면 controller 구현

--- src/main/java/umc/domain/home/controller/HomeController.java
+++ src/main/java/umc/domain/home/controller/HomeController.java
@@ -1,4 +1,26 @@
 package umc.domain.home.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import umc.domain.home.dto.HomeResDTO;
+import umc.domain.member.exception.code.MemberSuccessCode;
+import umc.global.apiPayload.ApiResponse;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api")
 public class HomeController {
+
+    //홈 화면
+    @GetMapping("/v1/home")
+    public ApiResponse<HomeResDTO.HomeDTO> getHome(
+            @RequestParam String region
+    ){
+        return ApiResponse.onSuccess(MemberSuccessCode.HOME_VIEW_SUCCESS, null);
+    }
+
 }
