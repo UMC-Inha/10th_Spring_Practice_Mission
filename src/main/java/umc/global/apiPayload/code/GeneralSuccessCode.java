0c8fff85c381df4fbaa41fc4976b903f22c4e336
LeeJaeJun1 <jj030207@naver.com>
2026-05-05 21:43:48 +0900

    refactor: GeneralSuccessCode가BaseSuccessCode를 상속하도록 변경

--- src/main/java/umc/global/apiPayload/code/GeneralSuccessCode.java
+++ src/main/java/umc/global/apiPayload/code/GeneralSuccessCode.java
@@ -7,7 +7,7 @@ import lombok.RequiredArgsConstructor;
 
 @Getter
 @RequiredArgsConstructor
-public enum GeneralSuccessCode {
+public enum GeneralSuccessCode implements BaseSuccessCode{
 
 	OK(HttpStatus.OK,
 		"COMMON201_1",
