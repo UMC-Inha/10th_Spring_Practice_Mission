5f2b1683c050bbeda9d5e25d7c7dbe5e998da236
LeeJaeJun1 <jj030207@naver.com>
2026-05-05 21:45:53 +0900

    feat: 각 도메인 entity 코드 추가

--- src/main/java/umc/domain/store/entity/Region.java
+++ src/main/java/umc/domain/store/entity/Region.java
@@ -1,4 +1,30 @@
 package umc.domain.store.entity;
 
-public class Region {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import umc.global.apiPayload.code.BaseEntity;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Table(name = "region")
+public class Region extends BaseEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = false)
+	private String name;
 }
--- src/main/java/umc/domain/store/entity/Store.java
+++ src/main/java/umc/domain/store/entity/Store.java
@@ -1,4 +1,58 @@
 package umc.domain.store.entity;
 
-public class Store {
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import umc.global.apiPayload.code.BaseEntity;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Table(name = "store")
+public class Store extends BaseEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "region_id", nullable = false)
+	private Region region;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "category_id", nullable = false)
+	private Food foodCategory;
+
+	@Column(nullable = false)
+	private String name;
+
+	@Column(nullable = false)
+	private Double storeAverage;
+
+	@Column(nullable = false)
+	private String address;
+
+	@Column(nullable = false)
+	private Integer startTime;
+
+	@Column(nullable = false)
+	private Integer endTime;
+
+	@Column(nullable = false)
+	private String ownerNumber;
 }
