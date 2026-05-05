fdf638f3fb19ca260b74c10753edc772ad30cd4e
migi0401 <migi111@naver.com>
2026-05-05 13:49:37 +0900

    feat: 회원 도메인 엔티티 추가

--- src/main/java/umc/domain/member/entity/Member.java
+++ src/main/java/umc/domain/member/entity/Member.java
@@ -1,4 +1,59 @@
 package umc.domain.member.entity;
 
-public class Member {
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import umc.domain.member.enums.Gender;
+import umc.domain.review.entity.Review;
+import umc.global.common.BaseEntity;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "member")
+public class Member extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "gender", nullable = false)
+    @Enumerated(EnumType.STRING)
+    @Builder.Default
+    private Gender gender = Gender.NONE;
+
+    @Column(name = "birth", nullable = false)
+    private LocalDate birth;
+
+    @Column(name = "address", nullable = false)
+    private String address;
+
+    @Column(name = "mail", nullable = false)
+    private String mail;
+
+    @Column(name = "phoneNumber", nullable = false)
+    private String phoneNumber;
+
+    @Column(name = "userPoint", nullable = false)
+    private Integer userPoint;
+
+    @Column(name = "social_uid", nullable = false)
+    private String socialUid;
+
+    @Column(name = "social_provider", nullable = false)
+    private String socialProvider;
+
+    @OneToMany(mappedBy = "member", cascade = CascadeType.ALL)
+    private List<Review> reviewLists = new ArrayList<>();
 }
