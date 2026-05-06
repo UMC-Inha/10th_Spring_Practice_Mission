3a89c80f8a5fd40859c522b3c5c55dff75c9d741
zeoueon <zeoueon129@gmail.com>
2026-05-06 23:05:23 +0900

    feat: member 도메인 엔티티 구현 및 enum 구현

--- src/main/java/umc/domain/member/entity/mapping/MemberTerm.java
+++ src/main/java/umc/domain/member/entity/mapping/MemberTerm.java
@@ -1,4 +1,34 @@
 package umc.domain.member.entity.mapping;
 
-public class MemberTerm {
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import umc.domain.member.entity.Member;
+import umc.domain.member.entity.Term;
+import umc.global.entity.BaseEntity;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "member_term")
+public class MemberTerm extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "term_id", nullable = false)
+    private Term term;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+    @Column(name = "is_agreed", nullable = false)
+    private Boolean isAgreed;
 }
--- src/main/java/umc/domain/member/enums/SocialType.java
+++ src/main/java/umc/domain/member/enums/SocialType.java
@@ -1,4 +1,16 @@
 package umc.domain.member.enums;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public enum SocialType {
+
+    KAKAO("카카오"),
+    NAVER("네이버"),
+    GOOGLE("구글"),
+    APPLE("애플");
+
+    private final String description;
 }
