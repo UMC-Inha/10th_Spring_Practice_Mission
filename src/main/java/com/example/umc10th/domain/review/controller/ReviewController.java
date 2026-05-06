6dd5fd0dbbc2bbb72ee444fa0b7d95ba8c53531d
ChoiMinSang <min393917@gmail.com>
2026-05-06 23:25:06 +0900

    feat: reivew controller

--- src/main/java/com/example/umc10th/domain/review/controller/ReviewController.java
+++ src/main/java/com/example/umc10th/domain/review/controller/ReviewController.java
@@ -1,4 +1,37 @@
 package com.example.umc10th.domain.review.controller;
 
+import com.example.umc10th.domain.review.dto.ReviewReqDto;
+import com.example.umc10th.domain.review.dto.ReviewResDto;
+import com.example.umc10th.domain.review.service.ReviewService;
+import com.example.umc10th.global.apiPayload.ApiResponse;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/stores/{storeId}/reviews")
 public class ReviewController {
+
+    private final ReviewService reviewService;
+
+    @GetMapping
+    public ApiResponse<ReviewResDto> getStoreReviews(
+            @PathVariable Long storeId,
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size
+    ) {
+        ReviewResDto response = reviewService.getStoreReviews(storeId, page, size);
+        return ApiResponse.onSuccess(response);
+    }
+
+    @PostMapping("/{reviewId}/reply")
+    public ApiResponse<Void> writeReviewReply(
+            @PathVariable Long storeId,
+            @PathVariable Long reviewId,
+            @Valid @RequestBody ReviewReqDto request
+    ) {
+        reviewService.writeReviewReply(storeId, reviewId, request);
+        return ApiResponse.onSuccess(null);
+    }
 }
