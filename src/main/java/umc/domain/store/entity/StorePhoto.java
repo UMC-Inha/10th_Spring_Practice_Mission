e705a3790515be091db589581879f14aa000b409
zeoueon <zeoueon129@gmail.com>
2026-05-06 23:07:38 +0900

    feat: store 도메인 엔티티 구현 및 enum 구현

--- src/main/java/umc/domain/store/entity/StorePhoto.java
+++ src/main/java/umc/domain/store/entity/StorePhoto.java
@@ -1,4 +1,28 @@
 package umc.domain.store.entity;
 
-public class StorePhoto {
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import umc.global.entity.BaseEntity;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "store_photo")
+public class StorePhoto extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "store_id", nullable = false)
+    private Store store;
+
+    @Column(name = "photo_url", nullable = false)
+    private String photoUrl;
 }
--- src/main/java/umc/domain/store/enums/StoreType.java
+++ src/main/java/umc/domain/store/enums/StoreType.java
@@ -1,4 +1,20 @@
 package umc.domain.store.enums;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public enum StoreType {
+
+    KOREAN("한식"),
+    CHINESE("중식"),
+    JAPANESE("일식"),
+    WESTERN("양식"),
+    CAFE("카페"),
+    FAST_FOOD("패스트푸드"),
+    BAR("술집"),
+    ETC("기타");
+
+    private final String description;
 }
