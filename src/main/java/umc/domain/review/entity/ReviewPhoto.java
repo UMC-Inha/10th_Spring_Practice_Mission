64a9eedf14f551c385ce71ac257a8f82dcb908bc
zeoueon <zeoueon129@gmail.com>
2026-05-06 23:07:01 +0900

    feat: review 도메인 엔티티 구현 및 enum 구현

--- src/main/java/umc/domain/review/entity/ReviewPhoto.java
+++ src/main/java/umc/domain/review/entity/ReviewPhoto.java
@@ -1,4 +1,28 @@
 package umc.domain.review.entity;
 
-public class ReviewPhoto {
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import umc.global.entity.BaseEntity;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "review_photo")
+public class ReviewPhoto extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "review_id", nullable = false)
+    private Review review;
+
+    @Column(name = "photo_url", nullable = false)
+    private String photoUrl;
 }
--- src/main/java/umc/domain/review/entity/ReviewReply.java
+++ src/main/java/umc/domain/review/entity/ReviewReply.java
@@ -1,4 +1,28 @@
 package umc.domain.review.entity;
 
-public class ReviewReply {
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import umc.global.entity.BaseEntity;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "review_reply")
+public class ReviewReply extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "review_id", nullable = false)
+    private Review review;
+
+    @Column(name = "content", nullable = false)
+    private String content;
 }
