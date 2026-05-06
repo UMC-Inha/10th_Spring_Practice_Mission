39b19ca09951d9e9f1eb9d658c0abe465c7bd3b1
zeoueon <zeoueon129@gmail.com>
2026-05-06 23:07:24 +0900

    feat: region 도메인 엔티티 구현 및 enum 구현

--- src/main/java/umc/domain/region/entity/Region.java
+++ src/main/java/umc/domain/region/entity/Region.java
@@ -1,4 +1,26 @@
 package umc.domain.region.entity;
 
-public class Region {
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import umc.domain.region.enums.RegionName;
+import umc.global.entity.BaseEntity;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "region")
+public class Region extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "name", nullable = false)
+    private RegionName name;
 }
