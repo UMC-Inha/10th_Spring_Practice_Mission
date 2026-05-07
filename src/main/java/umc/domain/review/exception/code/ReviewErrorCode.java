795d2b55ab6f04d51076f415c18a8c8c0fd0f7b1
yeongjun25 <ss91530441@gmail.com>
2026-05-07 10:44:43 +0900

    feat: 리뷰 작성 서비스 구현

--- src/main/java/umc/domain/review/exception/code/ReviewErrorCode.java
+++ src/main/java/umc/domain/review/exception/code/ReviewErrorCode.java
@@ -1,4 +1,16 @@
 package umc.domain.review.exception.code;
 
-public enum ReviewErrorCode {
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import umc.global.apiPayload.code.BaseErrorCode;
+
+@Getter
+@RequiredArgsConstructor
+public enum ReviewErrorCode implements BaseErrorCode {
+
+
+    private final HttpStatus status;
+    private final String code;
+    private final String message;
 }
--- src/main/java/umc/domain/review/exception/code/ReviewSuccessCode.java
+++ src/main/java/umc/domain/review/exception/code/ReviewSuccessCode.java
@@ -1,4 +1,17 @@
 package umc.domain.review.exception.code;
 
-public enum ReviewSuccessCode {
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import umc.global.apiPayload.code.BaseSuccessCode;
+
+@Getter
+@RequiredArgsConstructor
+public enum ReviewSuccessCode implements BaseSuccessCode {
+
+    REVIEW_CREATED(HttpStatus.OK, "REVIEW200_1", "리뷰가 성공적으로 작성되었습니다.");
+
+    private final HttpStatus status;
+    private final String code;
+    private final String message;
 }
