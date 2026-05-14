bfbcc381ddf8fe2c1c473079723da2b22d9b778b
zeoueon <zeoueon129@gmail.com>
2026-05-14 12:01:46 +0900

    feat: 검증 실패 예외 핸들러 추가
    - MethodArgumentNotValid
    - HandlerMethodValidation
    - ConstraintViolation

--- src/main/java/umc/global/apiPayload/handler/GeneralExceptionAdvice.java
+++ src/main/java/umc/global/apiPayload/handler/GeneralExceptionAdvice.java
@@ -1,12 +1,18 @@
 package umc.global.apiPayload.handler;
 
+import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.HandlerMethodValidationException;
 import umc.global.apiPayload.ApiResponse;
 import umc.global.apiPayload.code.BaseErrorCode;
 import umc.global.apiPayload.code.GeneralErrorCode;
 import umc.global.apiPayload.exception.GeneralException;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class GeneralExceptionAdvice {
@@ -33,5 +39,53 @@ public class GeneralExceptionAdvice {
                                 ex.getMessage()
                         )
                 );
+    }
+
+    // @Valid 어노테이션 검증 실패 예외
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiResponse<Map<String, String>>> handleMethodArgumentNotValidException(
+            MethodArgumentNotValidException e
+    ) {
+        // 검증 실패한 변수명과 실패 이유를 담을 Map
+        Map<String, String> errors = new HashMap<>();
+        e.getBindingResult().getFieldErrors().forEach(error ->
+            errors.put(error.getField(), error.getDefaultMessage())
+        );
+
+        BaseErrorCode code = GeneralErrorCode.BAD_REQUEST;
+        return ResponseEntity.status(code.getStatus())
+                .body(ApiResponse.onFailure(code, errors));
+    }
+
+    // 컨트롤러 @RequestParam, @PathVariable 등 검증 실패 예외
+    @ExceptionHandler(HandlerMethodValidationException.class)
+    public ResponseEntity<ApiResponse<Map<String, String>>> handleHandlerMethodValidationException(
+            HandlerMethodValidationException e
+    ) {
+        Map<String, String> errors = new HashMap<>();
+        e.getValueResults().forEach(result ->
+                result.getResolvableErrors().forEach(error ->
+                        errors.put(result.getMethodParameter().getParameterName(), error.getDefaultMessage())
+                )
+        );
+
+        BaseErrorCode code = GeneralErrorCode.BAD_REQUEST;
+        return ResponseEntity.status(code.getStatus())
+                .body(ApiResponse.onFailure(code, errors));
+    }
+
+    // @Validated + @RequestParam, @PathVariable 등 검증 실패 예외
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<ApiResponse<Map<String, String>>> handleConstraintViolationException(
+            ConstraintViolationException e
+    ) {
+        Map<String, String> errors = new HashMap<>();
+        e.getConstraintViolations().forEach(violation ->
+                errors.put(violation.getPropertyPath().toString(), violation.getMessage())
+        );
+
+        BaseErrorCode code = GeneralErrorCode.BAD_REQUEST;
+        return ResponseEntity.status(code.getStatus())
+                .body(ApiResponse.onFailure(code, errors));
     }
 }
