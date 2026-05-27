04485977a3f902bee4c0175b370ce41d8c146ad8
zeoueon <zeoueon129@gmail.com>
2026-05-27 13:13:33 +0900

    fix: 약관 빈 값 검증 추가

--- src/main/java/umc/domain/auth/dto/AuthReqDTO.java
+++ src/main/java/umc/domain/auth/dto/AuthReqDTO.java
@@ -1,10 +1,7 @@
 package umc.domain.auth.dto;
 
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.*;
 import umc.domain.member.enums.Gender;
 
 import java.util.List;
@@ -27,6 +24,7 @@ public class AuthReqDTO {
             @NotBlank
             String address,
             @Valid
+            @NotEmpty
             List<TermDTO> terms,
             @Valid
             List<FoodPreferenceDTO> foodPreferences
