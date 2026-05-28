79362733b4b6898dfd342db73c825481b520fc5f
andyoon <andyoon502@naver.com>
2026-05-28 19:48:40 +0900

    chore: 카카오 OAuth 설정

--- src/main/java/umc/global/config/SecurityConfig.java
+++ src/main/java/umc/global/config/SecurityConfig.java
@@ -1,11 +1,13 @@
 package umc.global.config;
 
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -14,6 +16,8 @@ import umc.global.security.exception.CustomAccessDenied;
 import umc.global.security.exception.CustomEntryPoint;
 import umc.global.security.exception.SecurityErrorResponseWriter;
 import umc.global.security.filter.JwtAuthFilter;
+import umc.global.security.handler.OAuthSuccessHandler;
+import umc.global.security.service.CustomOAuthService;
 import umc.global.security.service.CustomUserDetailsService;
 import umc.global.security.util.JwtUtil;
 
@@ -28,6 +32,7 @@ public class SecurityConfig {
     private final JwtUtil jwtUtil;
     private final CustomUserDetailsService customUserDetailsService;
     private final SecurityErrorResponseWriter securityErrorResponseWriter;
+    private final OAuthSuccessHandler oAuthSuccessHandler;
 
     private final String[] allowUris = {
             // Swagger 허용
@@ -35,11 +40,12 @@ public class SecurityConfig {
             "/swagger-resources/**",
             "/v3/api-docs/**",
 
-            "/public/**"
+            "/public/**",
+            "/oauth/**"
     };
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, CustomOAuthService customOAuthService) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(requests -> requests
@@ -56,7 +62,9 @@ public class SecurityConfig {
                 // JWT 필터
                 .addFilterBefore(jwtAuthFilter(), UsernamePasswordAuthenticationFilter.class)
                 // 세션
-                .sessionManagement(AbstractHttpConfigurer::disable)
+                .sessionManagement(
+                        AbstractHttpConfigurer::disable
+                )
                 .logout(logout -> logout
                         .logoutUrl("/logout")
                         .logoutSuccessUrl("/login?logout")
@@ -66,6 +74,21 @@ public class SecurityConfig {
                 .exceptionHandling(exception -> exception
                         .accessDeniedHandler(customAccessDenied)
                         .authenticationEntryPoint(customEntryPoint) // 전역 설정
+                )
+                // OAuth
+                .oauth2Login(oauth -> oauth
+                        .authorizationEndpoint(auth -> auth
+                                .baseUri("/oauth/authorize")
+                        )
+                        .redirectionEndpoint(redirect -> redirect
+                                .baseUri("/oauth/callback/**")
+                        )
+                        // 인증 완료 후 정보 활용
+                        .userInfoEndpoint(userInfo -> userInfo
+                                .userService(customOAuthService)
+                        )
+                        // 성공 시 JWT 토큰 발행할 핸들러
+                        .successHandler(oAuthSuccessHandler)
                 )
         ;
 
@@ -81,4 +104,5 @@ public class SecurityConfig {
     public JwtAuthFilter jwtAuthFilter(){
         return new JwtAuthFilter(jwtUtil, customUserDetailsService, securityErrorResponseWriter);
     }
+
 }
